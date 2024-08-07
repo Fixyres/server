b9aa66e6a13795110dbd510292d135b3260904dc
Артем <158840682+Fixyres@users.noreply.github.com>
2024-08-07 22:27:08 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,87 @@
+import telebot
+import paramiko
+from telebot import types
+
+TOKEN = '7218694009:AAG3KH-Z6SLzZsorYZkGZYvJ_UZ_tTGcoWg'
+SSH_HOST = 's2.serv00.com'
+SSH_PORT = 22
+SSH_USER = 'Foxy437'
+SSH_PASSWORD = '*P9*gNrMDWAL#1a1c9xb'
+
+AUTHORIZED_USERS = {1335063985}
+
+bot = telebot.TeleBot(TOKEN)
+
+def get_ssh_client():
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(SSH_HOST, port=SSH_PORT, username=SSH_USER, password=SSH_PASSWORD)
+    return ssh_client
+
+def execute_command(command):
+    ssh_client = get_ssh_client()
+    try:
+        full_command = f'clear; {command}'
+        stdin, stdout, stderr = ssh_client.exec_command(full_command)
+        output = stdout.read().decode() + stderr.read().decode()
+        if not output.strip():
+            output = '✅'
+    finally:
+        ssh_client.close()
+    return output
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.from_user.id
+    if user_id not in AUTHORIZED_USERS:
+        return None
+        
+    photo_path = 'cat.png'
+
+    if os.path.exists(photo_path):
+        with open(photo_path, 'rb') as photo:
+            bot.send_photo(
+                message.chat.id,
+                photo,
+                caption='*Приветиккк мой хозяин* `/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ `',
+                parse_mode='Markdown'
+            )
+    else:
+        bot.send_message(message.chat.id, '*Приветиккк мой хозяин* `/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ `', parse_mode='Markdown')
+
+@bot.message_handler(func=lambda m: True)
+def handle_message(message):
+    user_id = message.from_user.id
+    if user_id not in AUTHORIZED_USERS:
+        return None
+
+    command = message.text
+    output = execute_command(command)
+    bot.reply_to(message, output)
+
+@bot.inline_handler(lambda query: True)
+def handle_inline_query(inline_query):
+    user_id = inline_query.from_user.id
+    if user_id not in AUTHORIZED_USERS:
+        bot.answer_inline_query(inline_query.id, [])
+        return
+
+    query_text = inline_query.query
+    results = []
+
+    if query_text:
+        output = execute_command(query_text)
+        results.append(types.InlineQueryResultPhoto(
+            id='1',
+            photo_url='cat.png',
+            thumb_url='cat.png',
+            title='🐱 Отправить команду ฅ⁠^⁠•⁠ﻌ⁠•⁠^⁠ฅ.',
+            description='😽 Отправить команду котику.',
+            caption=output
+        ))
+
+    bot.answer_inline_query(inline_query.id, results)
+
+    bot.answer_inline_query(inline_query.id, results)
+
+bot.polling(none_stop=True)
