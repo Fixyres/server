93a58bf6067d236681277b3de8d0e098c5cd5421
Артем <158840682+Fixyres@users.noreply.github.com>
2024-08-07 22:44:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ def handle_inline_query(inline_query):
             thumbnail_url='cat.png',
             title='🐱 Отправить команду ฅ⁠^⁠•⁠ﻌ⁠•⁠^⁠ฅ.',
             description='😽 Отправить команду котику.',
-            WEBDOCUMENT_URL_INVALID='cat.png'
+            WEBDOCUMENT_URL_INVALID='https://drive.google.com/file/d/17crxuREHH3O96JyESAxzF1lVUYYAmWSe/view?usp=drivesdk'
         ))
 
     bot.answer_inline_query(inline_query.id, results)
